60564475d8a9200c4a48c30bcfc58ca995db281a
shinkar94 <romka94shinkarenko@gmail.com>
2023-11-08 23:36:16 +0300

    fix: fixed error for deploy

--- src/app/store/LibStore.ts
+++ src/app/store/LibStore.ts
@@ -3,11 +3,11 @@ import {immer} from "zustand/middleware/immer";
 import {devtools} from "zustand/middleware";
 
 export type LibType = {
-    "title": "CRA",
-    "code": "CRA",
-    "author": "romka94shinkarenko@gmail.com",
-    "idAuthor": "654bc13b001dbf1dc88c6ce4",
-    "id": "654be30b001dbf1dc88c6d14",
+    "title": string,
+    "code": string,
+    "author": string,
+    "idAuthor": string,
+    "id": string,
 }
 export interface ManagersStoreState {
     myHotLib: LibType[],
