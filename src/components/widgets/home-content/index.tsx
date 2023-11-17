581f15c9104781d8e82a279191753385ce4a15cf
shinkar90 <asuborisov@gmail.com>
2023-11-17 10:23:15 +0300

    feat: add workbox-window

--- src/components/widgets/home-content/index.tsx
+++ src/components/widgets/home-content/index.tsx
@@ -4,17 +4,39 @@ import s from './homeContent.module.scss'
 import Image from "next/image";
 import {UserDefault} from "@/components/shared";
 import {AddToHomeScreen} from "@/components/features";
+import {Workbox, WorkboxMessageEvent} from 'workbox-window';
+import {useEffect} from "react";
 
 export const HomeContent = () => {
     const user = useAuthStore((state) => state.user)
+
+    const wb = new Workbox('/sw.js');
+
+    const handleCacheUpdate = (event:WorkboxMessageEvent) => {
+        console.log("start EVENT")
+        if (event.data.type === 'CACHE_UPDATED') {
+            console.log("OPEN CHANGE")
+            const {updatedURL} = event.data.payload;
+            alert(`A newer version of ${updatedURL} is available!`);
+        }
+    }
+
+    useEffect(() => {
+        wb.addEventListener('message', handleCacheUpdate);
+
+        return () => {
+            wb.removeEventListener('message', handleCacheUpdate);
+        };
+    }, []);
+
     return (
         <div className={s.wrapper}>
             <div className={s.content}>
                 <div className={s.userBlock}>
                     {
                         user.avatarUrl
-                            ?<Image src={user.avatarUrl} alt={'userImg'} width={'80'} height={'80'} priority/>
-                            :<UserDefault />
+                            ? <Image src={user.avatarUrl} alt={'userImg'} width={'80'} height={'80'} priority/>
+                            : <UserDefault/>
                     }
                     <div className={s.infoUser}>
                         <h3>{user.fullName}</h3>
@@ -22,7 +44,7 @@ export const HomeContent = () => {
                     </div>
                     <p>test cache 4</p>
                 </div>
-                <AddToHomeScreen />
+                <AddToHomeScreen/>
             </div>
         </div>
     )
