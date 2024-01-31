83f854a1098cb39aeb28d5fc47c1fcf0261e5151
shinkar90 <asuborisov@gmail.com>
2024-01-31 11:41:41 +0300

    feat: new version video recorder

--- src/components/widgets/tetsing-content/index.tsx
+++ src/components/widgets/tetsing-content/index.tsx
@@ -2,6 +2,7 @@
 import s from './testingContent.module.scss'
 import {QrReaderC, VideoRecorder} from "@/components/features/ui";
 import {useState} from "react";
+import {VideoRecorderSecond} from "@/components/features/ui/video-recorder";
 export const TestingContent = () => {
     const [isQrScanner, setQrScanner] = useState(false)
     return(
@@ -10,8 +11,9 @@ export const TestingContent = () => {
             <VideoRecorder />
             <h3>QR reader</h3>
             <button onClick={()=> setQrScanner(true)}>open QrScanner</button>
-
             {isQrScanner && <QrReaderC callBack={()=> setQrScanner(false)}/>}
+            <h3>Video recorder v1.2</h3>
+            <VideoRecorderSecond />
         </div>
     )
 }
